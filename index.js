66ffdb7812c081bca9503d168a3b433a3799c661
Azeez Abdulrahman <azeezabdrayman@gmail.com>
2023-02-27 21:09:09 +0100

    javascript

---
+++ index.js
@@ -0,0 +1,10 @@
+let billInput = document.getElementById("bill-input")
+const tipBtn = document.getElementsByClassName("tip-btn")
+let peopleInput = document.getElementById("people-input")
+const reset = document.getElementById("second-part-button")
+
+let tipAmount = document.getElementById("tip-amount")
+let totalAmount = document.getElementById("total-amount")
+
+
+tipAmount = billInput;
